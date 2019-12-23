91825daaa43605d3379d522f941fbca3fd696bdf
Collins Kiprotich <collinsrono02@gmail.com>
2019-12-23 19:09:13 +0300

    Initial COmmit

---
+++ src/components/Data.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <div v-bind:style="divStyle">
+            <p v-bind:style="{color:textColor, textTransform: upperText, background:bgColor}">My VueJS paragraph</p>
+        </div>
+        <div>
+            <h3>Conditional Binding</h3>
+            <div v-if="myDate > 1 && myDate < 12">
+                <p>Good Morning</p>
+            </div>
+            <div v-else-if="myDate >=12 && myDate < 18">
+                <p>Good Afternoon</p>
+            </div>
+            <div v-else>
+                <p>Good Evening</p>
+            </div>
+        </div>
+        <div>
+            <h3>List Rendering</h3>
+            <div>
+                <ul v-for="task in myTasks" :key="task.id">
+                    <li>
+                        {{task.name}}
+                        <span v-if="task.completed">Completed</span> <span v-else>Not Completed</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Data",
+        data(){
+            const Dates = new Date();
+            const myDate = Dates.getHours();
+            return {
+                divStyle: {
+                    padding:'100px' ,
+                    background:'black',
+                    textAlign:'center'
+                },
+                textColor: 'red',
+                upperText: 'uppercase',
+                bgColor: 'yellow',
+                myDate: myDate,
+                myTasks: {
+                    task1:{
+                        id:1,
+                        name: 'task one',
+                        completed: false
+                    },
+                    task2:{
+                        id:2,
+                        name: 'task two',
+                        completed: false
+                    },
+                    task3:{
+                        id:3,
+                        name: 'task three',
+                        completed: false
+                    }
+                }
+        }
+        }
+    }
+</script>
+
+<style scoped>
+ul{
+    list-style: none;
+
+}
+    li{
+        background: aqua;
+        padding: 20px;
+    }
+    span{
+        color: red;
+    }
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <div>
+            <ul v-for="todos in todo" :key="todos.id">
+                <li>{{todos.name}}</li>
+            </ul>
+            <button @click="triggerAlert">Show Date</button>
+        </div>
+        <div>
+            <form>
+                <input type="text" placeholder="Enter Name" v-model="formDatas.myname"/>
+                <span>{{formDatas.myname}}</span>
+                <br>
+                <input type="text" placeholder="Enter Password" v-model="formDatas.passWord"/>
+                <span>{{formDatas.passWord}}</span>
+
+            </form>
+            <button @click="clearData">Clear</button>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Index',
+        data() {
+            return {
+                isDisabled: true,
+                myname:'',
+                passWord: '',
+                todo: {
+                  todo1:{
+                      id: 1,
+                      name: 'Task One',
+                  },
+                    todo2:{
+                      id: 2,
+                      name: 'My Name',
+                  },
+                },
+                formDatas:{}
+            }
+        },
+        methods: {
+            triggerAlert(){
+                const today = new Date();
+                const todays = today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear() ;
+                alert('Today is' + '  ' + todays)
+            },
+            clearData(){
+                this.formDatas = ''
+            }
+        }
+    }
+</script>
+
+<style>
+    .disabled{
+        background: aqua;
+
+    }
+    button{
+        padding: 10px;
+        margin: 10px;
+        background: blueviolet;
+        color: aliceblue;
+
+    }
+    input{
+        width: 400px;
+        padding: 8px;
+        margin: 10px;
+    }
+</style>
+
+
